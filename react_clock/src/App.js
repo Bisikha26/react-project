94ad6feec7ddb9d790cb446f38b844201dbabbe8
Bisikha <bisikhasubedi26@gmail.com>
2021-05-23 19:29:05 +0545

    digital clock

---
+++ react_clock/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DateTime from './components/Clock';
+
+function App() {
+  return (
+    <div className="App">
+     <DateTime></DateTime>
+    </div>
+  );
+}
+
+export default App;
